122088bf5f7aefa282004a2a2d9826fef8f42200
SeanCh7 <62511636+SeanCh7@users.noreply.github.com>
2024-01-28 13:06:15 -0600

    Add score sending

--- server/main.py
+++ server/main.py
@@ -7,11 +7,17 @@ m_matchScore = score.MatchScore()
 
 def msgHandler(msg):
     if msg['type'] == 'addScore':
-        m_matchScore.addScore(msg['data']['alliance'], msg['data']['location'])
+        m_matchScore.addScore(msg['data']['alliance'], msg['data']['location'], msg['data']['score'])
         return 0
 
 async def main() :
     async with serve(msgHandler, 'localhost', 8700) as server:
         await asyncio.Future()
 
+async def sendScore():
+    while True:
+        m_matchScore.updateArena()
+        await asyncio.sleep(0.1)
+
 asyncio.run(main())
+asyncio.run(sendScore())
