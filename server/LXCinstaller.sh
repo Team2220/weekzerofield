c9e6e7ceb540937bc01d57e64baf040c8e6edbb3
SeanCh7 <62511636+SeanCh7@users.noreply.github.com>
2024-01-31 12:42:56 -0600

    Set up for deployment

---
+++ server/LXCinstaller.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt update
+apt dist-upgrade -y
+apt install git -y
+
+sudo apt-get install python3.9 -y
+
+mkdir /weekzerofield
+cd /weekzerofield
+
+git clone https://github.com/Team2220/weekzerofield.git
+yes | python3 -m pip install -r weekzerofield/requirements.txt
